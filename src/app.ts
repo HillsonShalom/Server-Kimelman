740f31f5d2d8791aab9ac14217d407267dd4bef6
Hillson <hillsonshalom@gmail.com>
2024-11-11 11:47:44 +0200

    routes-basic

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,19 @@
-console.log("hello ts-node");
+import exp from "express";
+import cors from "cors";
+import "dotenv/config";
+import cookieParser from "cookie-parser";
+import accountRouter from "./routes/auth.router";
+import electionRouter from "./routes/election.router";
+const port = process.env.PORT;
+
+const app = exp();
+app.use(cors());
+app.use(exp.json());
+app.use(cookieParser());
+
+app.use("/account", accountRouter);
+app.use("/election", electionRouter);
+
+app.listen(port || 5000, () => {
+  console.log(`Listening on port ${port}...`);
+});
